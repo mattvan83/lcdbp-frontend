40e4d0c7b658929cd4798062e46e5b72693aea8c
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-04-26 20:51:35 +0200

    Install react-bootstrap-icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import {
+  HouseFill,
+  EnvelopeFill,
+  GearFill,
+  PersonFill,
+} from "react-bootstrap-icons";
 
 export default function Header() {
   return (
