f990ba20d3a783c3ac9816d4528d6e243f35918c
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-04-26 20:26:41 +0200

    Update react bootstrap navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,27 +26,77 @@ export default function Header() {
             <p className={styles.userTitle}>Espace membres</p>
           </div>
         </div>
-        <Navbar expand="sm" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
+        <Navbar expand="lg" className={styles.navbarBootstrap}>
+          <Container className={styles.navbarContainer}>
+            <Navbar.Brand href="#home" className={styles.navbarBrand}>
+              LCDBP
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse
+              id="navbarScroll"
+              className={styles.navbarCollapse}
+            >
+              <Nav className={`ms-auto ${styles.navbarNav}`} navbarScroll>
+                <NavDropdown
+                  title="Accueil"
+                  id="navbarScrollingDropdown"
+                  className={styles.navbarDropdown}
+                  active={false}
+                >
+                  <NavDropdown.Item as={Link} href="#action/3.1">
+                    Action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} href="#action/3.2">
                     Another action
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
+                  <NavDropdown.Item as={Link} href="#action/3.3">
                     Something
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
+                  <NavDropdown.Item as={Link} href="#action/3.4">
                     Separated link
                   </NavDropdown.Item>
                 </NavDropdown>
+                <Nav.Link
+                  as={Link}
+                  href="#link"
+                  className={styles.navbarLink}
+                  active={false}
+                >
+                  Le Chœur
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  href="#link"
+                  className={styles.navbarLink}
+                  active={false}
+                >
+                  Évènements
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  href="#link"
+                  className={styles.navbarLink}
+                  active={false}
+                >
+                  Nous Écouter
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  href="#link"
+                  className={styles.navbarLink}
+                  active={false}
+                >
+                  Revue de Presse
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  href="#link"
+                  className={styles.navbarLink}
+                  active={false}
+                >
+                  Nous contacter
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
