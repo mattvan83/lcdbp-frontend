0eedb669fb8b92aa90294a3a14c40032fa88a3ca
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2025-01-22 19:28:39 +0100

    Add control on query parameters for authentication on the /admin page and routes

--- src/app/(admin)/admin/[[...nextadmin]]/page.tsx
+++ src/app/(admin)/admin/[[...nextadmin]]/page.tsx
@@ -3,11 +3,31 @@ import { getNextAdminProps } from "@premieroctet/next-admin/appRouter";
 import { prisma } from "@/lib/prisma";
 import { options } from "@/lib/options";
 import "../../globals.css"; // .css file containing tailiwnd rules
+import { redirect } from "next/navigation";
 
 // console.log("Prisma instance:", prisma);
 
 export default async function AdminPage({ params, searchParams }: PageProps) {
   // or PromisePageProps for Next 15+
+
+  const userToken = searchParams?.["token"];
+
+  if (!userToken) {
+    redirect("/");
+  }
+
+  // Check if user exists and is admin
+  const user = await prisma.users.findFirst({
+    where: {
+      token: userToken as string,
+      type: "admin",
+    },
+  });
+
+  if (!user) {
+    redirect("/");
+  }
+
   const props = await getNextAdminProps({
     params: params.nextadmin,
     searchParams,
--- src/app/(admin)/api/admin/[[...nextadmin]]/route.ts
+++ src/app/(admin)/api/admin/[[...nextadmin]]/route.ts
@@ -9,6 +9,29 @@ const { run } = createHandler({
   prisma,
   /*options*/
   options,
+  onRequest: async (req: Request) => {
+    const url = new URL(req.url);
+    const { searchParams } = url;
+
+    // Access query parameters
+    const userToken = searchParams.get("token");
+
+    if (!userToken) {
+      return Response.json({ error: "Forbidden" }, { status: 403 });
+    }
+
+    // Check if user exists and is admin
+    const user = await prisma.users.findFirst({
+      where: {
+        token: userToken as string,
+        type: "admin",
+      },
+    });
+
+    if (!user) {
+      return Response.json({ error: "Forbidden" }, { status: 403 });
+    }
+  },
 });
 
 export { run as DELETE, run as GET, run as POST };
